00dd75085a58be1b0bd4d23913ebf0bf1f0ec106
Zain Saqib <135007714+Xain1501@users.noreply.github.com>
2025-06-17 22:02:13 +0500

    Update StudentReportCardGenerator.py

--- Week1/StudentReportCardGenerator.py
+++ Week1/StudentReportCardGenerator.py
@@ -20,3 +20,72 @@
 # Grade: A
 # • Read and Display the Report Card
 
+
+def CalAvg(marks):
+  Total=0
+  for i in marks.values():
+    Total+= i
+  
+  Avg= Total/len(marks)
+  return Avg
+
+def CalGrade():
+  if Avg>=80:
+    Grade='A'
+  elif Avg>=60:
+    Grade='B'
+  elif Avg>=40:
+    Grade='C'
+  else:
+    Grade ='F'
+  return Grade
+
+
+def WriteReport(name,age,Grade,Avg,marks):
+  fd=open("report.txt","a")
+  fd.write("Name:" + name)
+  fd.write("Age" + str(age))
+  fd.write("Grade" + Grade)
+  fd.write("Average" + str(Avg))
+  for sub, mark in marks.items:
+   fd.write(f"{sub}: {mark}\n")
+
+  fd.write("\n\n")
+
+  
+
+def ReadReport():
+  fd= open("report.txt","r")
+  for line in fd:
+    print(line.strip())
+    fd.close()
+
+
+name= input("Enter Your Name: ")
+age= int(input("Enter Your Age: "))
+marks={}
+subs=["English","Math","Urdu"]
+
+for key in subs:
+  mark=float(input("Enter marks: "))
+  marks[key]= mark
+
+Avg= CalAvg(marks)
+Grade =CalGrade(Avg)
+WriteReport(name,age,Grade,Avg,marks)
+ReadReport()
+
+
+ask= lambda: input("Do you want to give bonus marks? (y/n): ")
+Bonus=ask()
+
+if Bonus=='y':
+  for key,val in marks:
+    marks[key]= val+5
+
+
+
+
+Avg= CalAvg(marks)
+Grade =CalGrade(Avg)
+
