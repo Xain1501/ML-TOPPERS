5860b2c7a82908369b4c664e607a16660c6dcd07
Zain Saqib <135007714+Xain1501@users.noreply.github.com>
2025-06-17 21:01:50 +0500

    Update StudentReportCardGenerator.py
    Added project description and requirements

--- Week1/StudentReportCardGenerator.py
+++ Week1/StudentReportCardGenerator.py
@@ -1,12 +1,22 @@
+# MIni PROJECT 1
+# requirements:
+# • Take Input from the User (name and age)
+# • Take Input For 3 Subjects in a dictionary subject:marks (english, math, urdu)
+# • Define a Function to Calculate Average of all subjects together
+# • Grade Assignment using if/elif/else
+# Average Score Grade
+# 80 or above A
+# 60 to 79 B
+# 40 to 59 C
+# Below 40 F
+# • Bonus give using lambda function (Ask the user: Add 5 bonus marks to all subjects? (yes/no))
+# • Write the Report to a File
+# Name: Ali
+# Age: 20
+# Math: 90
+# English: 83
+# Urdu: 95
+# Average: 89.33
+# Grade: A
+# • Read and Display the Report Card
 
-# Basic Python program to add two numbers entered by the user
-
-# Get input from the user
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-# Add the numbers
-sum = num1 + num2
-
-# Display the result
-print("The sum of", num1, "and", num2, "is:", sum)
